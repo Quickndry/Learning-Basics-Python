744650634e1a518b269e5be17815ca6ec3ee7093
60590314 <Quickndry@users.noreply.github.com>
2020-12-22 00:01:17 +0100

    Update bootup.py

--- adventofcode/2020/handheld/bootup.py
+++ adventofcode/2020/handheld/bootup.py
@@ -6,39 +6,55 @@ def splitfile(textfile):
     with open(textfile) as document:
         lines = document.readlines()
         list_of_lines = [x.strip() for x in lines]
-
-        print(list_of_lines)
-
     return(list_of_lines)
 
 def split_by_pattern(oneline):
     oneline_string = oneline[:5] + ' ' + oneline[5:]
-    oneline_list = split(oneline_string, ' ')
+    oneline_list = oneline_string.split(' ')
     return(oneline_list)
 
 def splitlist(list_of_lines):
+    list_of_oneliners = []
     for i in list_of_lines:
-        split_by_pattern(i)
+        list_of_oneliners.append(split_by_pattern(i))
+    return list_of_oneliners
 
-def bootupseq(oneline):
+def bootupseq(list_of_oneliners):
     global indexposition
+    global accumulator
 
+    oneline = list_of_oneliners[indexposition]
+    print(oneline)
 
-    if oneline_list[0] == 'acc':
-        accumulator += str(oneline_list[2])
-        indexposition += 1
-
-    elsif oneline_list[0] == 'jmp':
-        indexposition += oneline_list[2]
+    if len(oneline) == 4:
+        print(accumulator)
+        return(accumulator)
 
     else:
-        indexposition += 1
+        if oneline[0] == 'acc':
+            if oneline[1] == '-':
+                accumulator -= int(oneline[2])
+                indexposition += 1
+                oneline.append('marker')
+            else:
+                accumulator += int(oneline[2])
+                indexposition += 1
+                oneline.append('marker')
 
-    bootupseq(s)
+        elif oneline[0] == 'jmp':
+            if oneline[1] == '-':
+                indexposition -= int(oneline[2])
+                oneline.append('marker')
+            else:
+                indexposition += int(oneline[2])
+                oneline.append('marker')
 
+        else:
+            indexposition += 1
+            oneline.append('marker')
 
+    bootupseq(list_of_oneliners)
 
-    #regex oneline
-    if
-
-    pass
+list_of_lines = splitfile("bootup.txt")
+list_of_oneliners = splitlist(list_of_lines)
+bootupseq(list_of_oneliners)
