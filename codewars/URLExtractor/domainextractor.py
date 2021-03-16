7abb9c073f3f6670952c20d58891f0e248938a65
Raphael G <60590314+Quickndry@users.noreply.github.com>
2021-03-16 01:13:36 +0100

    update

--- codewars/URLExtractor/domainextractor.py
+++ codewars/URLExtractor/domainextractor.py
@@ -3,41 +3,30 @@
 import re
 
 def domain_name(url):
-    urlComponents = ["http:", "https:", "com", "net", "co", "jp"]
-
-    urlContent = re.split('[/.]', url)
-
-    domainList = [x for x in urlContent if x not in urlComponents]
-
-    domain = domainList[0]
-
-    print(domainList)
-    print(domain)
-
-
-
-
-import re
-
-def domain_name(url):
     print(url)
-    urlcontent = re.split('[/.]', url)
-    print(urlcontent) #creates URL components list
     
-    splist = urlcontent[:len(urlcontent)//2]
-    splisttwo = urlcontent[len(urlcontent)//2:]
-    print(splist) #creates new list out of first halve
-    
-    possurl = max(splist, key=len) 
-    print(type(possurl))
-    print(possurl) #takes longest word and hope's its a URL
-    
-    if possurl == "http:" or "https:":
-		#if possurl not found in first halve, try second halve
-        possurl = max(splisttwo, key=len)
-        print(possurl) #takes the longest word and hopes it's a url
+    fullstopElements = []
+    dashElements = []
 
-    else:
-        pass
+
+    counter = 0
+
+    for i in url:
+        counter += 1
+        if i == '.':
+            fullstopElements.append(counter)
+        elif i == '/':
+            dashElements.append(counter + 1)
+        else:
+            pass
     
-    return possurl
+    
+    if 'www' in url:
+        domain = url[fullstopElements[0]:fullstopElements[1] - 1] 
+    elif 'http' in url:
+        domain = url[dashElements[0]:fullstopElements[0] - 1]
+    else:
+        domain = url[:fullstopElements[0] - 1]
+    
+    print(domain)
+    return(domain)
