79d84460f04856ba5014edc4cd5f0ed4e03384ca
Raphael G <60590314+Quickndry@users.noreply.github.com>
2021-12-13 01:52:30 +0000

    Update parttwo_smoke.py
    works

--- adventofcode/2021/Day9-Smoke/parttwo_smoke.py
+++ adventofcode/2021/Day9-Smoke/parttwo_smoke.py
@@ -1,18 +1,14 @@
 def basin(list_of_strings, list_of_coordinates, list_of_dupes, size):
 	if len(list_of_coordinates) == 0:
-#		print("Final Size: ", size)
 		return size + 1
 	else:
-#		print("List of coordinates: ", list_of_coordinates, "\n")
 		new_list_coordinates = []
 		new_size = size
 		for i in range(len(list_of_coordinates)):
-#			print("\ni: ", i, "\nlist_of_coordinates[i]: ", list_of_coordinates[i])
 			coordinates = list_of_coordinates[i]
 			list_of_dupes.append(coordinates)
 			above_id = [coordinates[0] - 1, coordinates[1]]
 			if above_id in list_of_dupes:
-#				print("Above in dupes.")
 				pass
 			else:
 				if above_id[0] == -1:
@@ -25,7 +21,6 @@ def basin(list_of_strings, list_of_coordinates, list_of_dupes, size):
 						new_size += 1
 						list_of_dupes.append(above_id)
 						new_list_coordinates.append(above_id)
-#						print("Above ID: ", above_id, "\nAbove: ", above)
 
 			below_id = [coordinates[0] + 1, coordinates[1]]
 			if below_id in list_of_dupes:
@@ -41,15 +36,12 @@ def basin(list_of_strings, list_of_coordinates, list_of_dupes, size):
 						new_size += 1
 						list_of_dupes.append(below_id)
 						new_list_coordinates.append(below_id)
-#						print("Below ID: ", below_id, "\nBelow: ", below)
 
 			prior_id = [coordinates[0], coordinates[1] - 1]
 			if prior_id in list_of_dupes:
-#				print("pass")
 				pass
 			else:
 				if prior_id[1] == -1:
-#					print("-1")
 					list_of_dupes.append(prior_id)
 				else:
 					prior = list_of_strings[coordinates[0]][coordinates[1] - 1]
@@ -59,7 +51,6 @@ def basin(list_of_strings, list_of_coordinates, list_of_dupes, size):
 						new_size += 1
 						list_of_dupes.append(prior_id)
 						new_list_coordinates.append(prior_id)
-#						print("Prior ID: ", prior_id, "\nPrior: ", prior)
 
 			after_id = [coordinates[0], coordinates[1] + 1]
 			if after_id in list_of_dupes:
@@ -75,13 +66,10 @@ def basin(list_of_strings, list_of_coordinates, list_of_dupes, size):
 						new_size += 1
 						list_of_dupes.append(after_id)
 						new_list_coordinates.append(after_id)
-#						print("After ID: ", after_id, "\nAfter: ", after)
 
-#		print("New Coordinates: ", new_list_coordinates,"\nSize: ", new_size,  "\nDupes: ", list_of_dupes, "\n\n")
-		try:
-			return basin(list_of_strings, new_list_coordinates, list_of_dupes, new_size)
-		except RecursionError:
-			print("Recursion Error")
+
+		return basin(list_of_strings, new_list_coordinates, list_of_dupes, new_size)
+
 	
 def smoke(txtfile):
 	input_file = open(txtfile, "r")
@@ -89,7 +77,6 @@ def smoke(txtfile):
 	string_list = input_strings.split("\n")
 	number_of_strings = len(string_list)
 	basin_sizes = []
-	low_coordinates = [] #delete later
 
 	for string in string_list:
 		previous_id = string_list.index(string) - 1
@@ -98,44 +85,35 @@ def smoke(txtfile):
 		if previous_id == -1:
 			next_string = string_list[next_id]
 			for i, digit in enumerate(string):
-#				print("Digit: ", digit, "\nRound: ", i)
 				try:
 					if int(digit) < int(string[i-1]) and int(digit) < int(string[i+1]) and int(digit) < int(next_string[i]):
 						string_index = string_list.index(string)
-						character_index = string.index(digit)
+						character_index = i
 						lowcoordinates = [[string_index, character_index]]
 						low_coordinates.append(lowcoordinates)
 						duplicates = []
 						size = 0
-#						print("Active1")
 						basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#						print("Basin Size: ", basin_size)
 						basin_sizes.append(basin_size)
 				except IndexError:
 					try:
 						if int(digit) < int(string[i+1]) and int(digit) < int(next_string[i]):
 							string_index = string_list.index(string)
-							character_index = string.index(digit)
+							character_index = i
 							lowcoordinates = [[string_index, character_index]]
-							low_coordinates.append(lowcoordinates)
 							duplicates = []
 							size = 0
-#							print("Active2")
 							basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#							print("Basin Size: ", basin_size)
 							basin_sizes.append(basin_size)
 					except IndexError:
 						try:
 							if int(digit) < int(string[i-1]) and int(digit) < int(next_string[i]):
 								string_index = string_list.index(string)
-								character_index = string.index(digit)
+								character_index = i
 								lowcoordinates = [[string_index, character_index]]
-								low_coordinates.append(lowcoordinates)
 								duplicates = []
 								size = 0
-#								print("Active3")
 								basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#								print("Basin Size: ", basin_size)
 								basin_sizes.append(basin_size)
 						except IndexError:
 							pass
@@ -143,44 +121,35 @@ def smoke(txtfile):
 		elif next_id >= number_of_strings:
 			previous_string = string_list[previous_id]
 			for i, digit in enumerate(string):
-#				print("Digit: ", digit, "\nRound: ", i)
 				try:
 					if int(digit) < int(previous_string[i]) and int(digit) < int(string[i-1]) and int(digit) < int(string[i+1]):
 						string_index = string_list.index(string)
-						character_index = string.index(digit)
+						character_index = i
 						lowcoordinates = [[string_index, character_index]]
-						low_coordinates.append(lowcoordinates)
 						duplicates = []
 						size = 0
-#						print("Active4")
 						basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#						print("Basin Size: ", basin_size)
 						basin_sizes.append(basin_size)
 				except IndexError:
 					try:
 						if int(digit) < int(previous_string[i]) and int(digit) < int(string[i+1]):
 							string_index = string_list.index(string)
-							character_index = string.index(digit)
+							character_index = i
 							lowcoordinates = [[string_index, character_index]]
 							low_coordinates.append(lowcoordinates)
 							duplicates = []
 							size = 0
-#							print("Active5")
 							basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#							print("Basin Size: ", basin_size)
 							basin_sizes.append(basin_size)
 					except IndexError:
 						try:
 							if int(digit) < int(previous_string[i]) and int(digit) < int(string[i-1]):
 								string_index = string_list.index(string)
-								character_index = string.index(digit)
+								character_index = i
 								lowcoordinates = [[string_index, character_index]]
-								low_coordinates.append(lowcoordinates)
 								duplicates = []
 								size = 0
-#								print("Active6")
 								basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#								print("Basin Size: ", basin_size)
 								basin_sizes.append(basin_size)
 						except IndexError:
 							pass
@@ -189,52 +158,38 @@ def smoke(txtfile):
 			next_string = string_list[next_id]
 			previous_string = string_list[previous_id]
 			for i, digit in enumerate(string):
-#				print("Digit: ", digit, "\nRound: ", i)
 				try:
 					if int(digit) < int(previous_string[i]) and int(digit) < int(string[i-1]) and int(digit) < int(string[i+1]) and int(digit) < int(next_string[i]):
 						string_index = string_list.index(string)
-						character_index = string.index(digit)
+						character_index = i
 						lowcoordinates = [[string_index, character_index]]
-						low_coordinates.append(lowcoordinates)
 						duplicates = []
 						size = 0
-#						print("Active7")
 						basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#						print("test")
-#						print("Basin Size: ", basin_size)
 						basin_sizes.append(basin_size)
 				except IndexError:
 					try:
 						if int(digit) < int(previous_string[i]) and int(digit) < int(string[i+1]) and int(digit) < int(next_string[i]):
 							string_index = string_list.index(string)
-							character_index = string.index(digit)
+							character_index = i
 							lowcoordinates = [[string_index, character_index]]
-							low_coordinates.append(lowcoordinates)
 							duplicates = []
 							size = 0
-#							print("Active8")
 							basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#							print("Basin Size: ", basin_size)
 							basin_sizes.append(basin_size)
 					except IndexError:
 						try:
 							if int(digit) < int(previous_string[i]) and int(digit) < int(string[i-1]) and int(digit) < int(next_string[i]):
 								string_index = string_list.index(string)
-								character_index = string.index(digit)
+								character_index = i
 								lowcoordinates = [[string_index, character_index]]
-								low_coordinates.append(lowcoordinates)
 								duplicates = []
 								size = 0
-#								print("Active9")
 								basin_size = basin(string_list, lowcoordinates, duplicates, size)
-#								print("Basin Size: ", basin_size)
 								basin_sizes.append(basin_size)
 						except IndexError:
 							pass
 	contains_duplicates = any(low_coordinates.count(element) > 1 for element in low_coordinates)
-	print("contains duplicate: ", contains_duplicates)
-	print("How many low: ", len(low_coordinates), "\nHow many basins: ", len(basin_sizes))
-	print("Lowpoints: ", low_coordinates, "\nBasin Sizes: ", basin_sizes)
 	basin_sizes.sort(reverse=True)
 	final_output = basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
 	print("Lowpoints ordered: ", sorted(low_coordinates), "\nBasin Sizes: ", basin_sizes, "\nFinal Output", final_output)
