bf9a55e1ba6a092177a75260e67ab6750b109d0c
Raphael G <60590314+Quickndry@users.noreply.github.com>
2021-12-15 05:51:45 +0000

    Update parttwo_origami.py

--- adventofcode/2021/Day13-Origami/parttwo_origami.py
+++ adventofcode/2021/Day13-Origami/parttwo_origami.py
@@ -5,13 +5,18 @@ def stringsplitter(txtfile):
 
   temp_list_of_coordinates = temp_lst[0].split("\n")
   list_of_coordinates = [x.split(",") for x in temp_list_of_coordinates]
+  list_of_coordinates_int = []
+  for element in list_of_coordinates:
+    coordinate_integer = [int(x) for x in element]
+    list_of_coordinates_int.append(coordinate_integer)
 
   temp_list_of_instructions = temp_lst[1].split("\n")
   list_of_instructions = [inst[11:].split("=") for inst in temp_list_of_instructions]
 
   coordinates_and_instructions = []
-  coordinates_and_instructions.append(list_of_coordinates)
+  coordinates_and_instructions.append(list_of_coordinates_int)
   coordinates_and_instructions.append(list_of_instructions)
+  print("Coo and Inst: ", coordinates_and_instructions)
   return coordinates_and_instructions
 
 def fold_action(coordinate_lst, fold_instructions):
@@ -27,19 +32,19 @@ def fold_action(coordinate_lst, fold_instructions):
          else:
            difference = abs(int(coordinate[0]) - int(fold_instructions[0][1]))
            newx = int(fold_instructions[0][1]) - difference
-           newcoordinate = [str(newx), coordinate[1]]
+           newcoordinate = [int(newx), int(coordinate[1])]
            if newcoordinate not in newcoordinate_lst:
              newcoordinate_lst.append(newcoordinate)
 	
     elif fold_instructions[0][0] == "y":
       for coordinate in coordinate_lst:
         if int(coordinate[1]) < int(fold_instructions[0][1]):
-           if coordinate not in newcoordinate_lst:
+          if coordinate not in newcoordinate_lst:
              newcoordinate_lst.append(coordinate)
         else:
           difference = abs(int(coordinate[1]) - int(fold_instructions[0][1]))
           newy = int(fold_instructions[0][1]) - difference
-          newcoordinate = [coordinate[0], str(newy)]
+          newcoordinate = [int(coordinate[0]), int(newy)]
           if newcoordinate not in newcoordinate_lst:
             newcoordinate_lst.append(newcoordinate)
   
@@ -47,31 +52,40 @@ def fold_action(coordinate_lst, fold_instructions):
     return fold_action(newcoordinate_lst, newfold_instructions)
 
   else:
+    print("\nCoordinate List: ", coordinate_lst)
     return coordinate_lst
 
 def final_origami(final_coordinates):
-  sortby_x = final_coordinates.sort(reverse=True)
-  reverse_coordinates = [x.reverse() for x in final_coordinates]
-  sortby_y = reverse_coordinates.sort(reverse=True)
-  length_of_string = int(sortby_x[0][0])
-  number_of_strings = int(sortby_y[0][0])
-  newstring = "." * length_of_string
-  newstring_as_lst = newstring.split("")
+  reverse_coordinates = [x[::-1] for x in final_coordinates]
+# print("reve: ", reverse_coordinates)
+  final_coordinates.sort(reverse=True)
+  reverse_coordinates.sort(reverse=True)
+#  print("Sortbyx: ", final_coordinates, "\nSortbyy: ", final_coordinates)
+  length_of_string = int(final_coordinates[0][0]) + 1
+  number_of_strings = int(reverse_coordinates[0][0]) + 1
+  newstring = ". " * length_of_string
+  newstring_as_lst = newstring.split(" ")
   field = []
   finalfield = []
 
+#  print("Y length: ", number_of_strings, "\nX length: ", length_of_string)
   for i in range(number_of_strings):
     field.append(newstring_as_lst)
   
   for coordinates in final_coordinates:
-    field[int(coordinates[1])][int(coordinates[0])] = ["X"]
+#    print("Coordinates: ", coordinates)
+    field[int(coordinates[1])][int(coordinates[0])] = "X"
 
   for lines in field:
+ #   print("Line: ", lines)
     line = "".join(lines)
     finalfield.append(line)
   
   finalfield_str = "\n".join(finalfield)
-
+  
+  text_file = open("output.txt", "w")
+  n = text_file.write(finalfield_str)
+  text_file.close()
   
 
 
