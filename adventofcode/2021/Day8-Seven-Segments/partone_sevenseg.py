4cf2cd6938194b2832fb85cdeb885d7aead04413
Raphael G <60590314+Quickndry@users.noreply.github.com>
2021-12-20 02:25:18 +0000

    Update partone_sevenseg.py

--- adventofcode/2021/Day8-Seven-Segments/partone_sevenseg.py
+++ adventofcode/2021/Day8-Seven-Segments/partone_sevenseg.py
@@ -6,14 +6,14 @@ def smoke(txtfile):
   for string in string_list:
     split_string = string.split("|")
     output_values = split_string[1].split(" ")
-    for output in output_value:
+    for output in output_values:
       final_output_values.append(output)
   
   counter = 0
   for output in final_output_values:
-    if len(output) == 2 or len(output) == 3 or len(output) == 4 or len(output) == 8:
+    if len(output) == 2 or len(output) == 3 or len(output) == 4 or len(output) == 7:
       counter += 1
   
   print("Number of specific digits: ", counter)
   
- smoke("input.txt")
+smoke("input.txt")
