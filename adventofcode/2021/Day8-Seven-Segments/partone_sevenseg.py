e83ad1253d8545a3d965e59074490d5c5e17354c
Raphael G <60590314+Quickndry@users.noreply.github.com>
2021-12-20 02:07:37 +0000

    Create partone_sevenseg.py

---
+++ adventofcode/2021/Day8-Seven-Segments/partone_sevenseg.py
@@ -0,0 +1,19 @@
+def smoke(txtfile):
+  input_file = open(txtfile, "r")
+  input_strings = input_file.read()
+  string_list = input_strings.split("\n")
+  final_output_values = []
+  for string in string_list:
+    split_string = string.split("|")
+    output_values = split_string[1].split(" ")
+    for output in output_value:
+      final_output_values.append(output)
+  
+  counter = 0
+  for output in final_output_values:
+    if len(output) == 2 or len(output) == 3 or len(output) == 4 or len(output) == 8:
+      counter += 1
+  
+  print("Number of specific digits: ", counter)
+  
+ smoke("input.txt")
